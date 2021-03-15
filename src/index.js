0d270644c528d9d02661b78b081888d2b886ec21
wallinthefuture <wallinthefuture@mail.ru>
2021-03-15 15:06:41 +0300

    feat: add brackets balans

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    if (str.length % 2 != 0) return false;
+
+    let strLength = Math.floor(str.length / 2);
+    let bracketsLength = bracketsConfig.length;
+
+    for (let pos = 0; pos < strLength; pos++) {
+        for (let config = 0; config < bracketsLength; config++) {
+            let strConfig = bracketsConfig[config].join("");
+            str = str.replace(strConfig, "");
+        }
+    }
+
+    if (str.length > 0) return false;
+    else return true;
+};
